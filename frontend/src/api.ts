44422d4103120a3faf5142e46ebe89d03e19fa53
luisxmetalx <junitho_93@hotmail.com>
2026-02-28 22:43:08 -0500

    Front. cambios en el front para poder usar el api

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { getToken, clearToken } from "./auth";
 
-const baseURL =
-  import.meta.env.VITE_API_BASE_URL ||
-  (import.meta.env.DEV ? "http://127.0.0.1:8000" : "");
+// const baseURL =
+//   import.meta.env.VITE_API_BASE_URL ||
+//   (import.meta.env.DEV ? "http://127.0.0.1:8000" : "");
 
-export const api = axios.create({ baseURL });
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
 
 api.interceptors.request.use((config) => {
   const token = getToken();
